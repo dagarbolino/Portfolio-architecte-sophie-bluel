811d7d935aefbe870c98fde1cf1338df63ea629e
alexandre dnk <101813042+dagarbolino@users.noreply.github.com>
2023-10-13 00:39:56 +0200

    filtre des bouton, tableau récupéré

--- FrontEnd/assets/js/script.js
+++ FrontEnd/assets/js/script.js
@@ -1,17 +1,70 @@
 const url = "http://localhost:5678/api/";
 
-const getCategories = async () => {
-  fetch(url + "categories")
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      console.log(data);
-    })
-    .catch ((error) => {
-      console.log(error);
-    });
-
+const getWorks = async () => {
+  try {
+    const response = await fetch(url + "works");
+    const data = await response.json();
+    //console.table(data);
+    return data; // Retourne les données récupérées
+  } catch (error) {
+    console.log(error);
+    throw new Error('Erreur lors de la récupération des données');
+  }
 };
 
-getCategories();
+
+const boutonAll = document.querySelector(".btnTous");
+
+boutonAll.addEventListener("click", async function () {
+  try {
+    const worksData = await getWorks(); //  Les données des travaux
+    const filteredCategories = worksData.filter(function (work) {
+      return work.categoryId === 1, 2, 3; //  Les travaux
+    });
+    console.log(filteredCategories); // Affiche les travaux dans la console
+  } catch (error) {
+    console.log(error); // Affiche les erreurs dans la console
+  }
+});
+
+
+const boutonObjets = document.querySelector(".btnObjets");
+boutonObjets.addEventListener("click", async function () {
+  try {
+    const worksData = await getWorks(); // Les données des travaux
+    const filteredCategories = worksData.filter(function (work) {
+      return work.categoryId === 1; // Filtre les travaux par categoryId 
+    });
+    console.log(filteredCategories); // Affiche les travaux filtrés dans la console
+  } catch (error) {
+    console.log(error); // Affiche les erreurs dans la console
+  }
+});
+
+
+const boutonAppartements = document.querySelector(".btnAppartements");
+boutonAppartements.addEventListener("click", async function () {
+  try {
+    const worksData = await getWorks(); // Les données des travaux
+    const filteredCategories = worksData.filter(function (work) {
+      return work.categoryId === 2; // Filtre les travaux par categoryId 
+    });
+    console.log(filteredCategories); // Affiche les travaux filtrés dans la console
+  } catch (error) {
+    console.log(error); // Affiche les erreurs dans la console
+  }
+});
+
+
+const boutonHotelsRestos = document.querySelector(".btnHotelsRestos");
+boutonHotelsRestos.addEventListener("click", async function () {
+  try {
+    const worksData = await getWorks(); // Les données des travaux
+    const filteredCategories = worksData.filter(function (work) {
+      return work.categoryId === 3; // Filtre les travaux par categoryId 
+    });
+    console.log(filteredCategories); // Affiche les travaux filtrés dans la console
+  } catch (error) {
+    console.log(error); // Affiche les erreurs dans la console
+  }
+});
